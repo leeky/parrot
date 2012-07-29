4cef644f3b7ee83a1db0474fc7e6edbb1ef707a9
Robert Lee-Cann <leeky@leeky.org.uk>
2012-07-29 09:54:03 +0100

    Add Rails 3.2.7 and all my usual gems for testing...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parrot::Application.config.secret_token = '225112d2f1c90229d8878d1a5024ddc3e2f2a16630b2cd22b4bc4f6912565a06f3f99409bcbbe8e27de7362f0bc41ea2b9336bfc0fdd0a8a9125ff22fecbca0c'
